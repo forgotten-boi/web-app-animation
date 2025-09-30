cad2c3933fcc88d6324b1a6c96ff80ac514d4034
Pitambar Jha <nishjha4u@gmail.com>
2025-09-30 16:56:07 +0100

    Delete multi directory

--- multi/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.transition = "background 2s";
-  setInterval(() => {
-    document.body.style.background = `hsl(${Math.random()*360}, 50%, 90%)`;
-  }, 5000);
-});
